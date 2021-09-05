c4f1941529cc0cbb5877999d2bf48227b6e9047e
Andreev Alexey <andreev-j@yandex.ru>
2021-09-05 13:27:43 +0700

    update1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,33 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	    List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        List<Integer> newList = new ArrayList<>();
-        for(Integer num : intList) {
-            if (num > 0 && num % 2 == 0) newList.add(num);
+        int size = 0;
+        int num;
+        int[] numbers = {1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4};
+        int[] rez = new int[numbers.length-1];
+        // фильтр
+        for(int a : numbers) {
+            if (a > 0 && a % 2 == 0) {
+                rez[size] = a;
+                size++;
+            }
         }
-        newList.sort(Comparator.naturalOrder());
-        newList.forEach(System.out::println);
-
+        // сортировка
+        for (int i = 0; i < size-1; i++){
+            for (int j = i; j >= 0; j--){
+                if (rez [j]> rez [j+1]) {
+                    num = rez[j];
+                    rez [j] = rez [j+1];
+                    rez [j+1] = num;
+                }
+            }
+        }
+        // вывод на экран
+        System.out.println("\n***** arrays *****\n");
+        for(int i = 0; i < size; i++) {
+            System.out.println(rez[i]);
+        }
+        // с потоками
         System.out.println("\n***** StreamMain *****\n");
         StreamMain.main();
     }
